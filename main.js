968d47b708335407394cda596779725687632995
Muhammadm2211 <100311046+Muhammadm2211@users.noreply.github.com>
2022-02-26 13:34:16 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.gallery__slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/previous.png" alt="" width="24"></button>', 
+        nextArrow: '<button type="button" class="slick-next"><img src="images/next.png" alt="" width="24"></button>',
+        vertical: true
+    });
+});
